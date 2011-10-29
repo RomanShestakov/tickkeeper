a8ce213c75539f6ed8730b44a99e70e506420c02
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-10-29 23:04:31 +0100

    init commit

---
+++ src/bf_tsdb_util.erl
@@ -0,0 +1,39 @@
+%% Copyright (C) 2011 Roman Shestakov
+%%%
+%%% This file is part of bf_bot
+%%%
+%%% bf_bot is free software: you can redistribute it and/or modify
+%%% it under the terms of the GNU Lesser General Public License as 
+%%% published by the Free Software Foundation, either version 3 of 
+%%% the License, or (at your option) any later version.
+%%%
+%%% bf_bot is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%% GNU Lesser General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU Lesser General Public 
+%%% License along with Erlsom.  If not, see 
+%%% <http://www.gnu.org/licenses/>.
+%%%
+%%% Author contact: romanshestakov@yahoo.co.uk
+
+-module(bf_tsdb_util).
+
+-export([
+	 log4erl_config/0
+	]).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Get log config of the project
+%% creates a full path name to the job file definition.
+%% @end
+%%--------------------------------------------------------------------
+-spec log4erl_config() -> string() | no_return().
+log4erl_config() ->
+    case application:get_env(bf_tsdb, log4erl_config) of
+	{ok, Value} -> Value;
+	undefined -> throw({error, log4erl_config_not_defined})
+    end.
+
