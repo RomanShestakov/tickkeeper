5ddac9cd0e069743f431a26064f170ec3167122d
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-11-02 23:34:25 +0000

    add test for diff schema

--- test/bf_tsdb_tests.erl
+++ test/bf_tsdb_tests.erl
@@ -5,7 +5,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -define(SCHEMA, [{"timestamp", integer}, {"bid", float}, {"ask", float}]).
-
+-define(SCHEMA1, [{"timestamp", integer}, {"name", integer}, {"bid", float}, {"ask", float}]).
 
 process_test_() ->
     {setup, local,
@@ -31,8 +31,11 @@ run(_P) ->
       ?_assertMatch({error, db_already_exists}, bf_tsdb:create("test_db", ?SCHEMA)),
       ?_assertMatch({error, db_already_open}, bf_tsdb:open("test_db")),
       ?_assertMatch({error,{db_not_exist, _}}, bf_tsdb:open("test_db1")),
+      ?_assertMatch(ok, bf_tsdb:create("test_db1", ?SCHEMA1)),
       ?_assertMatch(ok, bf_tsdb:close("test_db")),
-      ?_assertMatch({error, db_not_open}, bf_tsdb:close("test_db"))
+      ?_assertMatch(ok, bf_tsdb:close("test_db1")),
+      ?_assertMatch({error, db_not_open}, bf_tsdb:close("test_db")),
+      ?_assertMatch({error, db_not_open}, bf_tsdb:close("test_db1"))
      ]}.
 
 
